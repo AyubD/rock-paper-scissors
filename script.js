b50f095b20a2089dd4704888b13c7ad9d38d2fc4
AyubD <ayub_duale02089@hotmail.co.uk>
2022-10-23 08:29:53 +0100

    playRound function
    Created this function that allows me to play a signle round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function getComputerChoice() {
-    var item = ["Rock", "Paper", "Scissors"]
-    return item[Math.floor(Math.random()*item.length)]
-}
+    const item = ['rock', 'paper', 'scissors'];
+    return item[Math.floor(Math.random() * item.length)];
+ }
+ 
+ function playRound(playerSelection, computerSelection) {
+    if (console.log(playerSelection === computerSelection)) {
+       return `It's a tie! you both picked ${playerSelection}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+       return "You win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+       return "You win! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+       return "You win! Scissors beats Paper";
+    } else {
+       return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+ }
+ 
+ const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'").toLowerCase();
+ const computerSelection = getComputerChoice();
+ console.log(playRound(playerSelection, computerSelection))
